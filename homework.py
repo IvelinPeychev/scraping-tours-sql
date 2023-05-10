c456e012ccbabbcf71a2db3b02ab0f49cec87611
Ivelin Peychev <peychev.vn@gmail.com>
2023-05-10 11:58:22 +0300

    Day 39: - solve the homework by SQL DB approach

--- homework.py
+++ homework.py
@@ -3,9 +3,10 @@ import selectorlib
 import time
 import streamlit as st
 import plotly.express as px
+import sqlite3
 
 URL = 'https://programmer100.pythonanywhere.com/'
-
+connection = sqlite3.connect('data.db')
 
 def send_request(url):
     response = requests.get(url)
@@ -19,13 +20,29 @@ def extract_info(page_content):
 
 
 def store_info(info):
-    with open('homework.txt', 'a') as f:
-        f.write(info + '\n')
+    # In file
+    # with open('homework.txt', 'a') as f:
+    #     f.write(info + '\n')
+
+    # In DB
+    cursor = connection.cursor()
+    cursor.execute('INSERT INTO temperature VALUES(?,?)',info)
+    connection.commit()
+
+# def read_file(filename):
+    # From file
+    # with open(filename, 'r') as f:
+    #     return f.readlines()
 
 
-def read_file(filename):
-    with open(filename, 'r') as f:
-        return f.readlines()
+def read_db():
+    # From DB
+    cursor = connection.cursor()
+    cursor.execute('Select * FROM temperature')
+
+    rows = cursor.fetchall()
+    print(rows)
+    return rows
 
 
 if __name__ == '__main__':
@@ -40,22 +57,42 @@ if __name__ == '__main__':
     date_and_time = time.strftime('%y-%m-%d-%H-%M-%S')
     print(date_and_time)
 
-    # Store the date and temperature in a file
-    info = f"{date_and_time}, {temperature}"
+# TEXT FILE APPROACH
+#     # Store the date and temperature in a file
+#     info = f"{date_and_time}, {temperature}"
+#     store_info(info)
+#
+#     # Load the file as it will be needed for extract the values for the graph
+#     info_in_file = read_file('homework.txt')[1:]
+#
+#     # Extracting data needed for the graph
+#     dates = [row.split(', ')[0] for row in info_in_file]
+#     temperatures = [row.split(', ')[1] for row in info_in_file]
+#
+#     # Start the web app
+#     st.title('')
+#
+#     # Config the figure
+#     figure = px.line(x=dates, y=temperatures, labels={'x': 'Date', 'y': 'Temperature (C)'})
+#
+#     # Display the figure
+#     st.plotly_chart(figure)
+
+# SQL DB APPROACH
+    info = (temperature, date_and_time)
     store_info(info)
-
-    # Load the file as it will be needed for extract the values for the graph
-    info_in_file = read_file('homework.txt')[1:]
-
-    # Extracting data needed for the graph
-    dates = [row.split(', ')[0] for row in info_in_file]
-    temperatures = [row.split(', ')[1] for row in info_in_file]
+    graph_data = read_db()
+    temp = []
+    date = []
+    for data in graph_data:
+        temp.append(data[0])
+        date.append(data[1])
 
     # Start the web app
     st.title('')
 
     # Config the figure
-    figure = px.line(x=dates, y=temperatures, labels={'x': 'Date', 'y': 'Temperature (C)'})
+    figure = px.line(x=date, y=temp, labels={'x': 'Date', 'y': 'Temperature (C)'})
 
     # Display the figure
     st.plotly_chart(figure)
